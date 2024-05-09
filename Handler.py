d6630be6bb2cedd8eb0174faa9f2ea8a39969e77
Salma Elsoly <salmaelsoly@gmail.com>
2024-05-10 02:21:17 +0300

    Client-to-server done

--- Handler.py
+++ Handler.py
@@ -6,10 +6,12 @@ import Channel
 import websockets
 import cloudCredentials
 import asyncio
+import cv2
+import base64
 
 class RequestHandler(BaseHTTPRequestHandler):
-  def __init__(self, *args, channel, storage, **kwargs):
-    self.channel = channel
+  def __init__(self, *args,  storage, **kwargs):
+    self.channel = Channel.Channel()
     self.storage = storage
     super().__init__(*args, **kwargs)
 
@@ -21,29 +23,27 @@ class RequestHandler(BaseHTTPRequestHandler):
     else:
       client_address = self.client_address[0]
     post_data = self.rfile.read(content_length)
-    requests_data = json.loads(post_data)
-    results = []
-    for request in requests_data:
-      task_id = str(uuid.uuid4())
-      service_num = request['service_num']
-      encoded_image_as_str = request['image']
-      self.storage.upload_image(encoded_image_as_str, task_id)
-      message = {'client_address': client_address, 'task_id': task_id, 'service_num': service_num}
-      self.channel.publish('responses', json.dumps(message))
-      results.append(task_id)
+    request_data = json.loads(post_data)
+    task_id = str(uuid.uuid4())
+    service_num = request_data['service_num']
+    encoded_image_as_str = request_data['image']
+    self.storage.upload_image(encoded_image_as_str, task_id)
+    message = {'client_address': client_address, 'task_id': task_id, 'service_num': service_num}
+    self.channel.publish('responses', json.dumps(message))
+    result=task_id
     self.send_response(200)
     self.send_header('Content-type', 'application/json')
     self.end_headers()
-    self.wfile.write(json.dumps(results).encode())
+    self.wfile.write(json.dumps(result).encode())
 
 class Node:
-  def __init__(self, server_address,channel,storage):
+  def __init__(self, server_address,storage):
       self.server_address = server_address
-      self.channel = channel
+      self.channel = Channel.Channel()
       self.storage = storage
 
   def run(self):
-      httpd = HTTPServer(('localhost', 8000), lambda *args, **kwargs: RequestHandler(*args, channel=channel, storage=storage, **kwargs))
+      httpd = HTTPServer(('localhost', 8000), lambda *args, **kwargs: RequestHandler(*args, storage=storage, **kwargs))
       print(f"Starting server on {self.server_address}")
       httpd.serve_forever()
 
@@ -62,18 +62,20 @@ class Node:
     client_address = response['client_address']
     #get the url of the image from the cloud storage
     url = self.storage.create_signed_url(task_id)
-    asyncio.run(self.send_response(json.dumps({'task_id': task_id, 'url': url}), client_address))
+    image=storage.get_image(task_id)
+    _, encoded_image = cv2.imencode('.jpg', image)
+    encoded_image_as_str = base64.b64encode(encoded_image).decode('utf-8')
+    asyncio.run(self.send_response(json.dumps({'task_id': task_id, 'url': url,'image':encoded_image_as_str}), client_address))
     self.channel.ack(method)
 
 
   def ResponseHandler(self):
     print("Start consuming responses")
-    channel.consume('responses', self.callback)
+    self.channel.consume('responses', self.callback)
 
 if __name__ == "__main__":
-  channel=Channel.Channel()
   storage = cloudCredentials.Storage()
-  node = Node(('localhost', 8000),channel,storage)
+  node = Node(('localhost', 8000),storage)
   request_thread = Thread(target=node.run)
   response_thread = Thread(target=node.ResponseHandler)
   request_thread.start()
