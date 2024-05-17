39174a1d2385eebd4e87e2682f2f2b719be37583
Norhan Waleed <105253730+Norhan-salem@users.noreply.github.com>
2024-05-17 17:30:19 +0300

    not yet testeddd

--- load_test.py
+++ load_test.py
@@ -4,12 +4,11 @@ import time
 import random
 import json
 import requests
-from processing_node import ProcessingNode
 
 IMAGE_FILE_PATH = "testttt.png"
 
 def mock_upload(IMAGE_FILE_PATH, service_num):
-
+    start_time = time.time()
     with open(IMAGE_FILE_PATH, 'rb') as f:
         image_bytes = f.read()
     image_base64 = base64.b64encode(image_bytes).decode('utf-8')
@@ -17,31 +16,25 @@ def mock_upload(IMAGE_FILE_PATH, service_num):
     response = requests.post('http://localhost:8000', data=json.dumps(request),
                              headers={'Content-Type': 'application/json'})
     print(f'server response: {response.text}')
-
-def process_task(task_id, service_num):
-    start_time = time.time()
-    node = ProcessingNode()
-    node.run(task_id=task_id, service_num=service_num)
     end_time = time.time()
     return end_time - start_time
 
 
-def thread_function(service_num, times_list):
+def thread_function(times_list):
+
     service_num = random.choice(list(range(1, 5)) + list(range(6, 25)))  # Exclude service number 5
-    mock_upload(IMAGE_FILE_PATH, service_num)
-    #execution_time = process_task(task_id, service_num)
-    #times_list.append(execution_time)
+    execution_time = mock_upload(IMAGE_FILE_PATH, service_num)
+    times_list.append(execution_time)
 
 
 if __name__ == "__main__":
     threads = []
+    times_list = []
     for _ in range(10000):
         thread = threading.Thread(target=thread_function)
         threads.append(thread)
         thread.start()
 
-    times_list = []
-    threads = []
 
     for t in threads:
         t.join()
