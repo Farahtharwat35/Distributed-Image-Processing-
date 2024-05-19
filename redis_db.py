fe354dc5d44c40494af7b28e670e0b4ea520d02b
Salma Elsoly <salmaelsoly@gmail.com>
2024-05-19 05:19:56 +0300

    load testing done

--- redis_db.py
+++ redis_db.py
@@ -32,12 +32,12 @@ redisDB = RedisDB()
 
 # # Example usage:
 # # Assuming you have some task info to store
-task_id = "ba21c643-738e-4b78-b2ef-d093323c7a3a"
-task_info = {"status": "completed", "result": "success"}
+# task_id = "ba21c643-738e-4b78-b2ef-d093323c7a3a"
+# task_info = {"status": "completed", "result": "success"}
 
-# Storing task info
-redisDB.update_image_status(task_id, task_info)
+# # Storing task info
+# redisDB.update_image_status(task_id, task_info)
 
-# Retrieving task info
-task_data = redisDB.pull(task_id)
-print("Task data:", task_data)
+# # Retrieving task info
+# task_data = redisDB.pull(task_id)
+# print("Task data:", task_data)
